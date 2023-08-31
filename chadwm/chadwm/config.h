ddafbbb655ca4fa7826b00c3e39fbbcb727d8d93
Jezzrel <sjezzrey@gmail.com>
2023-08-31 16:50:41 +0800

    xuckless commit

--- chadwm/chadwm/config.h
+++ chadwm/chadwm/config.h
@@ -256,14 +256,14 @@ static const Key keys[] = {
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-    /* click                event mask      button          function        argument */
-    { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-    { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-    { ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = stcmd } },
+    /* click                event mask              button          function        argument */
+    { ClkLtSymbol,          0,                      Button1,        setlayout,      {0} },
+    { ClkLtSymbol,          0,                      Button3,        setlayout,      {.v = &layouts[2]} },
+    { ClkWinTitle,          0,                      Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,                      Button2,        spawn,          {.v = stcmd } },
 
     /* Keep movemouse? */
-    /* { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} }, */
+    /* { ClkClientWin,         MODKEY,              Button1,        movemouse,      {0} }, */
 
     /* placemouse options, choose which feels more natural:
     *    0 - tiled position is relative to mouse cursor
@@ -275,18 +275,18 @@ static const Button buttons[] = {
     * to control these separately (i.e. to retain the feature to move a tiled window
     * into a floating position).
     */
-    { ClkClientWin,         MODKEY,         Button1,        moveorplace,    {.i = 0} },
-    { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-    { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-    { ClkClientWin,         ControlMask,    Button1,        dragmfact,      {0} },
-    { ClkClientWin,         ControlMask,    Button3,        dragcfact,      {0} },
-    { ClkTagBar,            0,              Button1,        view,           {0} },
-    { ClkTagBar,            0,              Button3,        toggleview,     {0} },
-    { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-    { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-    { ClkTabBar,            0,              Button1,        focuswin,       {0} },
-    { ClkTabBar,            0,              Button1,        focuswin,       {0} },
-    { ClkTabPrev,           0,              Button1,        movestack,      { .i = -1 } },
-    { ClkTabNext,           0,              Button1,        movestack,      { .i = +1 } },
-    { ClkTabClose,          0,              Button1,        killclient,     {0} },
+    { ClkClientWin,         MODKEY,                 Button1,        moveorplace,    {.i = 0} },
+    { ClkClientWin,         MODKEY,                 Button2,        togglefloating, {0} },
+    { ClkClientWin,         MODKEY,                 Button3,        resizemouse,    {0} },
+    { ClkClientWin,         MODKEY|ShiftMask,       Button1,        dragmfact,      {0} },
+    { ClkClientWin,         MODKEY|ShiftMask,       Button3,        dragcfact,      {0} },
+    { ClkTagBar,            0,                      Button1,        view,           {0} },
+    { ClkTagBar,            0,                      Button3,        toggleview,     {0} },
+    { ClkTagBar,            MODKEY,                 Button1,        tag,            {0} },
+    { ClkTagBar,            MODKEY,                 Button3,        toggletag,      {0} },
+    { ClkTabBar,            0,                      Button1,        focuswin,       {0} },
+    { ClkTabBar,            0,                      Button1,        focuswin,       {0} },
+    { ClkTabPrev,           0,                      Button1,        movestack,      { .i = -1 } },
+    { ClkTabNext,           0,                      Button1,        movestack,      { .i = +1 } },
+    { ClkTabClose,          0,                      Button1,        killclient,     {0} },
 };
